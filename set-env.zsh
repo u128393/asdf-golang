21d9c329bb8e28cc7018fe90808e2bda751952d0
Eddie <egaldame@icloud.com>
2023-05-23 18:14:14 -0700

    Use built-in zsh function to get absolute dir path for golang (#105)
    Co-authored-by: Edwin Galdamez <egaldamez@gumgum.com>

--- set-env.zsh
+++ set-env.zsh
@@ -1,15 +1,9 @@
-function absolute_dir_path {
-    local absolute_path
-    absolute_path="$( cd -P "$( dirname "$1" )" && pwd )"
-    echo "$absolute_path"
-}
-
 asdf_update_golang_env() {
   local go_path
   go_path="$(asdf which go)"
   if [[ -n "${go_path}" ]]; then
     export GOROOT
-    GOROOT="$(dirname "$(absolute_dir_path "${go_path}")")"   
+    GOROOT="$(dirname "$(dirname "${go_path:A}")")"
   fi
 }
 
