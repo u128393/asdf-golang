d8dec15ccc0cffc2e2762f348ccffeffb79c6489
Sora Morimoto <sora@morimoto.io>
2023-07-19 22:32:53 +0900

    Hygiene (#113)
    * Add Makefile for Hygiene
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    
    * make format
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    
    ---------
    
    Signed-off-by: Sora Morimoto <sora@morimoto.io>
    Co-authored-by: Kenny Parnell <k.parnell@gmail.com>

--- lib/helpers.sh
+++ lib/helpers.sh
@@ -2,50 +2,50 @@
 set -eu
 [ "${BASH_VERSINFO[0]}" -ge 3 ] && set -o pipefail
 
-get_platform () {
-    local silent=${1:-}
-    local platform=""
+get_platform() {
+  local silent=${1:-}
+  local platform=""
 
-    platform="$(uname | tr '[:upper:]' '[:lower:]')"
+  platform="$(uname | tr '[:upper:]' '[:lower:]')"
 
-    case "$platform" in
-        linux|darwin|freebsd)
-            [ -z "$silent" ] && msg "Platform '${platform}' supported!"
-            ;;
-        *)
-            fail "Platform '${platform}' not supported!"
-            ;;
-    esac
+  case "$platform" in
+    linux | darwin | freebsd)
+      [ -z "$silent" ] && msg "Platform '${platform}' supported!"
+      ;;
+    *)
+      fail "Platform '${platform}' not supported!"
+      ;;
+  esac
 
-    printf "%s" "$platform"
+  printf "%s" "$platform"
 }
 
-get_arch () {
-    local arch=""
-    local arch_check=${ASDF_GOLANG_OVERWRITE_ARCH:-"$(uname -m)"}
-    case "${arch_check}" in
-        x86_64|amd64) arch="amd64"; ;;
-        i686|i386|386) arch="386"; ;;
-        armv6l|armv7l) arch="armv6l"; ;;
-        aarch64|arm64) arch="arm64"; ;;
-        ppc64le) arch="ppc64le"; ;;
-        *)
-            fail "Arch '${arch_check}' not supported!"
-            ;;
-    esac
+get_arch() {
+  local arch=""
+  local arch_check=${ASDF_GOLANG_OVERWRITE_ARCH:-"$(uname -m)"}
+  case "${arch_check}" in
+    x86_64 | amd64) arch="amd64" ;;
+    i686 | i386 | 386) arch="386" ;;
+    armv6l | armv7l) arch="armv6l" ;;
+    aarch64 | arm64) arch="arm64" ;;
+    ppc64le) arch="ppc64le" ;;
+    *)
+      fail "Arch '${arch_check}' not supported!"
+      ;;
+  esac
 
-    printf "%s" "$arch"
+  printf "%s" "$arch"
 }
 
-msg () {
-    echo -e "\033[32m$1\033[39m" >&2
+msg() {
+  echo -e "\033[32m$1\033[39m" >&2
 }
 
-err () {
-    echo -e "\033[31m$1\033[39m" >&2
+err() {
+  echo -e "\033[31m$1\033[39m" >&2
 }
 
-fail () {
-    err "$1"
-    exit 1
+fail() {
+  err "$1"
+  exit 1
 }
--- test-fixtures/create-dummy-installs.sh
+++ test-fixtures/create-dummy-installs.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 declare -a fake_install_versions=(
-    1.16
-    1.17.1
-    1.17.7
-    1.17.13
-    1.18.1
-    1.18.2
-    1.18.6
-    1.19
-    1.19.3
+  1.16
+  1.17.1
+  1.17.7
+  1.17.13
+  1.18.1
+  1.18.2
+  1.18.6
+  1.19
+  1.19.3
 )
 
 mkdir -p ../../installs/asdf-golang
 for version in "${fake_install_versions[@]}"; do
-    mkdir "../../installs/asdf-golang/${version}"
+  mkdir "../../installs/asdf-golang/${version}"
 done
